ad74cc77051eff957806b05e17cab59b1f5671d8
alfonsogalguera <ponchito.1021@hotmail.com>
2019-11-01 14:20:06 -0600

    QuetzarRage

---
+++ p03_base/src/scenes/Bootloader.js
@@ -0,0 +1,23 @@
+class Bootloader extends Phaser.Scene{
+    constructor(){
+        super({
+            key: "Bootloader" //Nombre interno o clave de referencia
+        });
+    }
+
+    init() {
+        console.log("Escena Bootloader");
+    }
+    preload() {
+        this.load.path = "./assets/";               //Ruta de todas las imagenes
+        this.load.image(["yoshi_fondo", "yoshi"]);  //Arreglo de imagenes
+    }
+    create() {
+        this.yoshi = this.add.image(100, 100, "yoshi"); //atributo
+    }
+    update(time, delta) {
+        // ESTA FUNCION CREA UN CICLO INFINITO
+    }
+}
+
+export default Bootloader;
